a269a43fe9f8c25c38c1c46bb5e33b538eb05693
HeniTech2 <henoka858@gmail.com>
2026-05-30 23:51:35 -0900

    Cleanup: remove duplicate nested Backend and Frontend folders

--- Backend/Frontend/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="py-8 text-center text-gray-400 border-t border-white/20">
-      <p>© 2026 Henok Amare. All rights reserved.</p>
-    </footer>
-  )
-}
-
-export default Footer
